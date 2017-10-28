2d8aca008b9e6895722fd2c2d88aafc6e92def59
Cleibert Mora <cleibertblog@gmail.com>
2017-10-28 11:13:42 -0500

    primer commit

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+
+{
+  //Generando la vista del index
+
+	public function index()
+	{
+
+		return view('posts.index');
+	
+	}
+
+	public function show($id)
+	{
+
+		return view('posts.show');
+
+	}
+
+}
